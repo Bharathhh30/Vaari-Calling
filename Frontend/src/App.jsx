984b57de1abe3a188135489b0c692f9eb2472be5
Bharathhh30 <vnsb30@gmail.com>
2024-12-28 22:18:59 +0530

    Project Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center align-middle p-12 text-2xl text-blue-300 font-bold'>Front-end of Vaari Calling</div>
+  )
+}
+
+export default App
